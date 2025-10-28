2b8e5736252e3ef76d890890821dfc3ba84dca52
phamegan <megpham556@gmail.com>
2025-10-27 21:52:26 -0700

    build and rest timers for hours mins and sec for sketch4

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -35,11 +35,37 @@ registerSketch('sk4', function (p) {
   };
   
   p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
+    // p.background(200, 240, 200);
+    // p.fill(30, 120, 40);
+    // p.textSize(32);
+    // p.textAlign(p.CENTER, p.CENTER);
+    // p.text('HWK #4. C', p.width / 2, p.height / 2);
+
+    // Build and reset state as time progresses
+    // Rebuild hours once per hour
+    if (h24 !== lastHour) {
+      circsH = [];
+      p.placeN(h12, wW/12, wH/6, 'hour', circsH)
+      lastHour = h24;
+    }
+
+    // Rebuild minutes once per minute
+    if (m !== lastMinute) {
+      circsM = [];
+      p.placeN(m, wW/14, wH/14, 'min', circsM)
+      lastMinute = m;
+    }
+
+    // Seconds: add one each second; reset at start of minute
+    if (s !== lastSecond) {
+      if (s === 0) {
+        circsS = []; // reset
+      } else {
+        p.placeOne(wW/32, wH/28, 'sec', circsS); // add exactly one
+      }
+      lastSecond = s;
+    }
+    
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
